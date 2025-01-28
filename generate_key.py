75f9e76c48e653a0c30ffbfa189dba62ef601af1
Meissa Babou <meissababou66@gmail.com>
2025-01-28 23:16:31 +0000

    Ajout du menu

--- generate_key.py
+++ generate_key.py
@@ -1,21 +1,30 @@
 from Crypto.Random import get_random_bytes
-from base64 import b64encode, decode
+from base64 import b64encode
+import sys
 
-key = get_random_bytes(16) # 128 bit key
-init_vector = get_random_bytes(16) # 128 bit IV
+def generate_key(key_size):
+    if key_size != 128 and key_size != 192 and key_size != 256:
+        print("La taille de la cle doit etre 128, 192 ou 256 bits")
+        sys.exit(1)
 
-key_base64 = b64encode(key).decode()
-init_vector_base64 = b64encode(init_vector).decode()
+    key = get_random_bytes(int(key_size / 8)) # 128 bit key
+    init_vector = get_random_bytes(16) # 128 bit IV
 
-key_name = input("Nom de la cle: ")
-if key_name.endswith(".key"):
-    key_location = "./keys/" + key_name
-else:
-    key_location = "./keys/" + key_name + ".key"
+    key_base64 = b64encode(key).decode()
+    init_vector_base64 = b64encode(init_vector).decode()
 
-try:
-    with open(key_location, "x") as key_f:
-        key_f.write(key_base64 + "\n" + init_vector_base64)
+    key_name = input("Nom de la cle: ")
 
-except FileExistsError:
-    print("Cette cle existe deja !")
+    if key_name.endswith(".key"):
+        key_location = "./keys/" + key_name
+    else:
+        key_location = "./keys/" + key_name + ".key"
+
+    try:
+        with open(key_location, "x") as key_f:
+            key_f.write(key_base64 + "\n" + init_vector_base64)
+            print("La cle a ete generee avec succes")
+
+    except FileExistsError:
+        print("Cette cle existe deja !")
+        sys.exit(1)
--- main.py
+++ main.py
@@ -3,19 +3,7 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 from base64 import b64encode, b64decode
 import sys
-
-# Load the key and IV from the file
-try:
-    with open("./keys/mykey.key", "r") as key_f:
-        lines = key_f.readlines()
-except:
-    print("Une erreur est survenue lors du chargement de la cle")
-    sys.exit(1)
-
-key_base64 = lines[0].strip()
-init_vector_base64 = lines[1].strip() 
-key = b64decode(key_base64)
-init_vector = b64decode(init_vector_base64)
+from generate_key import *
 
 # Encrypt function 
 def encrypt_f(file_location):
@@ -59,4 +47,57 @@ def decrypt_f(file_location):
         sys.exit(1)
 
 if __name__ == "__main__":
-    decrypt_f("./files/test.enc")
+    while True:
+        print("\nMenu Principal")
+        print("1. Générer une paire de clé/IV")
+        print("2. Chiffrer un fichier")
+        print("3. Déchiffrer un fichier")
+        print("4. Quitter")
+        choice = input("Choisissez une option (1-4) : ")
+        while choice not in ["1", "2", "3", "4"]:
+            choice = input("Choisissez une option valide (1-4) : ")
+        
+        if choice == "1":
+            key_size = int(input("Taille de la cle (128, 192, 256) : "))
+            generate_key(key_size)
+
+        elif choice == "2":
+            key_location = input("Chemin de la cle : ")
+
+            # Load the key and IV from the file
+            try:
+                with open(key_location, "r") as key_f:
+                    lines = key_f.readlines()
+            except:
+                print("Une erreur est survenue lors du chargement de la cle")
+                sys.exit(1)
+
+            key_base64 = lines[0].strip()
+            init_vector_base64 = lines[1].strip() 
+            key = b64decode(key_base64)
+            init_vector = b64decode(init_vector_base64)
+            file_location = input("Chemin du fichier a chiffrer : ")
+
+            encrypt_f(file_location)
+
+        elif choice == "3":
+            key_location = input("Chemin de la cle : ")
+
+            # Load the key and IV from the file
+            try:
+                with open(key_location, "r") as key_f:
+                    lines = key_f.readlines()
+            except:
+                print("Une erreur est survenue lors du chargement de la cle")
+                sys.exit(1)
+
+            key_base64 = lines[0].strip()
+            init_vector_base64 = lines[1].strip() 
+            key = b64decode(key_base64)
+            init_vector = b64decode(init_vector_base64)
+
+            file_location = input("Chemin du fichier a dechiffrer : ")
+            decrypt_f(file_location)
+
+        else:
+            sys.exit(0)
